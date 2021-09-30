61f1c21994afa34fbd2f1eb6451b0c440f647dea
Siegfred <dante_sparda1925@yahoo.com>
2021-10-01 02:48:06 +0300

    created a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,77 @@
 import React from 'react'
+import {FaFacebook,FaInstagram, FaTwitter } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <div>
-            FOOTER
+        <div className="footer text-light">
+            <div className="footer-links">
+                <div className="social-media-links">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <div className="social-media-items pb-5">
+                                    <div className="social-media-title">
+                                        <h1 className="text-center">You can find us at:</h1>
+                                        <div className="social-media-icons">
+                                            <div className="social-media-icon fs-2 text-light"><FaFacebook/></div>
+                                            <div className="social-media-icon fs-2 text-light ps-5"><FaInstagram/></div>
+                                            <div className="social-media-icon fs-2 text-light ps-5"><FaTwitter/></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="about-links ">
+                            <div className="row text-center pb-5 ">
+                                <div className="col-4"> 
+                                    <div className="footer-column1 border-border-danger">
+                                        <div className="column-title">
+                                            <h4 className="title">Frequently Searched</h4>
+                                        </div>
+                                        <div className="column-links">
+                                            <div className="column-item">Link 1 Lorem Ipsum</div>
+                                            <div className="column-item">Link 2 Lorem Ipsum</div>
+                                            <div className="column-item">Link 3 Lorem Ipsum</div>
+                                            <div className="column-item">Link 4 Lorem Ipsum</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-4"> 
+                                    <div className="footer-column2 border-border-danger">
+                                        <div className="column-title">
+                                            <h4 className="title">About Grab Eat</h4>
+                                        </div>
+                                        <div className="column-links">
+                                            <div className="column-item">Link 1 Lorem Ipsum</div>
+                                            <div className="column-item">Link 2 Lorem Ipsum</div>
+                                            <div className="column-item">Link 3 Lorem Ipsum</div>
+                                            <div className="column-item">Link 4 Lorem Ipsum</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-4"> 
+                                    <div className="footer-column3 border-border-danger">
+                                        <div className="column-title">
+                                            <h4 className="title">Support</h4>
+                                        </div>
+                                        <div className="column-links">
+                                            <div className="column-item">Link 1 Lorem Ipsum</div>
+                                            <div className="column-item">Link 2 Lorem Ipsum</div>
+                                            <div className="column-item">Link 3 Lorem Ipsum</div>
+                                            <div className="column-item">Link 4 Lorem Ipsum</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="copyright">
+                                    <div className="row">
+                                        <h6 className="copyright-texts text-light text-center">&copy; <em id="date"></em>Grab Eat 2021</h6>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
