e207a9f4a2a0aaf0b957e45dc29f2aec3814b693
Siegfred <dante_sparda1925@yahoo.com>
2021-09-30 07:47:25 +0300

    hardcoded a popular restaurant component slider

---
+++ src/components/PopularRestaurants.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Slider from './Slider'
+
+
+const PopularRestaurants = () => {
+    return (
+        <div className="popular-restaurants-component">
+            <div className="header-popular px-4">
+                <h1>Popular Restaurants</h1>
+            </div>
+            <Slider/>
+         
+        </div>
+    )
+}
+
+export default PopularRestaurants
