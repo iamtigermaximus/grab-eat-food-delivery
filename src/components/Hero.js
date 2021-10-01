81f7b98db7372a338050048bf2136d778afb72d0
Siegfred <dante_sparda1925@yahoo.com>
2021-10-01 06:09:24 +0300

    .

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,14 +8,14 @@ const Hero = () => {
                 <div className="row">
                     <div className="col-lg-6 col-md-6">
                         <div className="p-5">
-                            <div className="location-container text-center px-5">
+                            <div className="location-container text-center">
                                 <div className="heading-content rounded px-4 pb-5 bg-light">
                                     <h3 className="heading-content-text p-3">
                                     Let's explore good food in your area.
                                     </h3>
-                                    <div className="search-location mx-auto ">
+                                    <div className="search-location">
                                         <i className="text-danger fs-5"><IoLocationSharp/></i>
-                                        <input className="form-control-lg form-control-md form-control-sm" placeholder="Type your location" />
+                                        <input className="form-control-lg" placeholder="Type your location" />
                                     </div>
                                     <div className="button py-2 text-center">
                                         <button className="btn-search-location">Search</button>
