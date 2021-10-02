fe493221dc19c6c44fdcdb7e3b17d7b8221518e2
Siegfred <dante_sparda1925@yahoo.com>
2021-10-02 14:28:51 +0300

    new slider component

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,112 +1,30 @@
 import React from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import Image1 from '../images/food-image1.jpg'
-import Image2 from '../images/food-image2.jpg'
-import Image3 from '../images/food-image3.jpg'
-import Image4 from '../images/food-image4.jpg'
+import Carousel from 'react-elastic-carousel'
+import data from '../data/db.json'
+import SingleCard from './SingleCard'
+
+
 const Slider = () => {
+    const breakPoints = [
+        
+        { width: 480, itemsToShow: 2},
+        { width: 576, itemsToShow: 4 },
+        
+      ]
+    
     return (
-        <div className="carousel-wrapper">
-            <Carousel
-                showIndicators={false}
-                showStatus={false}
-            >
-                    <div className="cards-wrapper">
-                        <div className="card-container">
-                            <div className="card-image-container">
-                                <img className="image mx-2" src={Image1} alt="project" />
-                        </div>
-                        <div className="card-body">
-                            <div className="card-name">Restaurant Name 1</div>
-                            <div className="card-item">description</div>
-                            <div className="card-item">description 2</div>
-                    </div>
-                    </div>
-                        
-                    <div className="card-container">
-                            <div className="card-image-container">
-                                <img className="image mx-2" src={Image2} alt="project" />
-                        </div>
-                        <div className="card-body">
-                            <div className="card-name">Restaurant Name 2</div>
-                            <div className="card-item">description</div>
-                            <div className="card-item">description 2</div>
-                    </div>
-                        </div>
-                        
-                    <div className="card-container">
-                            <div className="card-image-container">
-                                <img className="image mx-2" src={Image3} alt="project" />
-                        </div>
-                        <div className="card-body">
-                            <div className="card-name">Restaurant Name 3</div>
-                            <div className="card-item">description</div>
-                            <div className="card-item">description 2</div>
-                    </div>
-                        </div>
-                        
-                    <div className="card-container">
-                            <div className="card-image-container">
-                                <img className="image mx-2" src={Image4} alt="project" />
-                        </div>
-                        <div className="card-body">
-                            <div className="card-name">Restaurant Name 4</div>
-                            <div className="card-item">description</div>
-                            <div className="card-item">description 2</div>
-                        </div>
-                        </div>
-                        
-                </div>
-                 <div className="cards-wrapper">
-                        <div className="card-container">
-                            <div className="card-image-container">
-                                <img className="image mx-2" src={Image1} alt="project" />
-                        </div>
-                        <div className="card-body">
-                            <div className="card-name">Restaurant Name 5</div>
-                            <div className="card-item">description</div>
-                            <div className="card-item">description 2</div>
-                    </div>
-                        </div>
-                        
-                    <div className="card-container">
-                            <div className="card-image-container">
-                                <img className="image mx-2" src={Image2} alt="project" />
-                        </div>
-                        <div className="card-body">
-                            <div className="card-name">Restaurant Name 6</div>
-                            <div className="card-item">description</div>
-                            <div className="card-item">description 2</div>
-                    </div>
-                        </div>
-                        
-                    <div className="card-container">
-                            <div className="card-image-container">
-                                <img className="image mx-2" src={Image3} alt="project" />
-                        </div>
-                        <div className="card-body">
-                            <div className="card-name">Restaurant Name 7</div>
-                            <div className="card-item">description</div>
-                            <div className="card-item">description 2</div>
-                    </div>
-                        </div>
-                        
-                    <div className="card-container">
-                            <div className="card-image-container">
-                                <img className="image mx-2" src={Image4} alt="project" />
-                        </div>
-                        <div className="card-body">
-                            <div className="card-name">Restaurant Name 8</div>
-                            <div className="card-item">description</div>
-                            <div className="card-item">description 2</div>
-                        </div>
-                        </div>
-                        
-                    </div>
-                </Carousel> 
-            </div>
+        <Carousel breakPoints={breakPoints} showArrows={false} pagination={false}>
+            {data.popular.map((pop) => (
+                <SingleCard
+                    id={pop.id}
+                    name={pop.name}
+                    description={pop.description}
+                />
+            ))}
+       </Carousel>
+        
     )
 }
 
 export default Slider
+
