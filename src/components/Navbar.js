20948c0fd8a7ee0cb0cef8e4b27c10dff1b7ae3a
Siegfred <dante_sparda1925@yahoo.com>
2021-10-03 11:57:19 +0300

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,8 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {FaBars, FaShoppingCart } from 'react-icons/fa'
 import { Link} from 'react-router-dom'
 
 const Navbar = () => {
-   
- 
     return (
         <div className="nav active">
             <div className="burger text-light">
