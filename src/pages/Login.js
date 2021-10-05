69f828f4b8f1da9d89a4fd612462178f16ee4be2
Siegfred <dante_sparda1925@yahoo.com>
2021-10-05 08:23:16 +0300

    added icon in the login component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaApple,FaGoogle, FaFacebook } from "react-icons/fa";
 
 const Login = () => {
     return (
@@ -13,9 +14,9 @@ const Login = () => {
                                 <h6>Sign up or log in to continue</h6>
                             </div>
                             <div className="login-links p-5">
-                                    <button className="button-login-link my-2 bg-dark text-light fw-bolder">CONTINUE WITH APPLE</button>
-                                    <button className="button-login-link my-2 bg-white text-danger border border-danger fw-bold">CONTINUE WITH GOOGLE</button>
-                                <button className="button-login-link my-2 bg-primary text-light fw-bold">CONTINUE WITH FACEBOOK</button>
+                                    <button className="button-login-link my-2 bg-dark text-light fw-bolder"><span className="fs-5 mx-2"><FaApple/></span>CONTINUE WITH APPLE</button>
+                                    <button className="button-login-link my-2 bg-white text-danger border border-danger fw-bold"><span className="fs-5 mx-2"><FaGoogle/></span>CONTINUE WITH GOOGLE</button>
+                                <button className="button-login-link my-2 bg-primary text-light fw-bold"><span className="fs-5 mx-2"><FaFacebook/></span>CONTINUE WITH FACEBOOK</button>
                                 <h5 className="text-center"> or </h5>
                                 <button className="button-login-link my-2 fw-bold border border-success text-success">CONTINUE WITH EMAIL</button>
                             </div>
