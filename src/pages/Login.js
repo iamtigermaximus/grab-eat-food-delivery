84b856338661aa07a70d81049e0c46be628cdbdf
Siegfred <dante_sparda1925@yahoo.com>
2021-10-05 08:26:52 +0300

    changed google icon

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {FaApple,FaGoogle, FaFacebook } from "react-icons/fa";
-
+import {FaApple,FaFacebook } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 const Login = () => {
     return (
         <div className="login">
@@ -15,7 +15,7 @@ const Login = () => {
                             </div>
                             <div className="login-links p-5">
                                     <button className="button-login-link my-2 bg-dark text-light fw-bolder"><span className="fs-5 mx-2"><FaApple/></span>CONTINUE WITH APPLE</button>
-                                    <button className="button-login-link my-2 bg-white text-danger border border-danger fw-bold"><span className="fs-5 mx-2"><FaGoogle/></span>CONTINUE WITH GOOGLE</button>
+                                    <button className="button-login-link my-2 bg-white text-danger border border-danger fw-bold"><span className="fs-5 mx-2"><FcGoogle/></span>CONTINUE WITH GOOGLE</button>
                                 <button className="button-login-link my-2 bg-primary text-light fw-bold"><span className="fs-5 mx-2"><FaFacebook/></span>CONTINUE WITH FACEBOOK</button>
                                 <h5 className="text-center"> or </h5>
                                 <button className="button-login-link my-2 fw-bold border border-success text-success">CONTINUE WITH EMAIL</button>
