dd5a55c271714fbc039c9b806473ae4b6f14f645
Siegfred <dante_sparda1925@yahoo.com>
2021-10-15 20:06:28 +0300

    added images in fake database

---
+++ src/data/db.js
@@ -0,0 +1,326 @@
+import Image1 from '../images/food-image1.jpg'
+import Image2 from '../images/food-image2.jpg'
+import Image3 from '../images/food-image3.jpg'
+import Image4 from '../images/food-image4.jpg'
+import Image5 from '../images/food-image5.jpg'
+import Image6 from '../images/food-image6.jpg'
+import Image7 from '../images/food-image7.jpg'
+import Image8 from '../images/food-image8.jpg'
+import Image9 from '../images/food-image9.jpg'
+import Image10 from '../images/food-image10.jpg'
+import Image11 from '../images/food-image11.jpg'
+import Image12 from '../images/food-image12.jpg'
+
+const data = {
+
+    "popular":[
+        {
+            "id":1,
+            "name":"Restaurant 1",
+            "description":"Lorem ipsum lorem",
+            "rating": 10,
+            "image": Image1
+            
+        },
+        {
+            "id":2,
+            "name":"Restaurant 2",
+            "description":"Lorem ipsum lorem",
+            "rating": 9,
+            "image": Image2
+        },
+        {
+            "id":3,
+            "name":"Restaurant 3",
+            "description":"Lorem ipsum lorem",
+            "rating": 8,
+            "image": Image3
+        },
+        {
+            "id":4,
+            "name":"Restaurant 4",
+            "description":"Lorem ipsum lorem",
+            "rating": 7,
+            "image": Image4
+        },{
+            "id":5,
+            "name":"Restaurant 5",
+            "description":"Lorem ipsum lorem",
+            "rating": 6,
+            "image": Image5
+        },
+        {
+            "id":6,
+            "name":"Restaurant 6",
+            "description":"Lorem ipsum lorem",
+            "rating": 5,
+            "image": Image6
+        },
+        {
+            "id":7,
+            "name":"Restaurant 7",
+            "description":"Lorem ipsum lorem",
+            "rating": 4,
+            "image": Image7
+        },
+        {
+            "id":8,
+            "name":"Restaurant 8",
+            "description":"Lorem ipsum lorem",
+            "rating": 3,
+            "image": Image8
+        },
+        {
+            "id":9,
+            "name":"Restaurant 9",
+            "description":"Lorem ipsum lorem",
+            "rating": 2,
+            "image": Image9
+        },
+        {
+            "id":10,
+            "name":"Restaurant 10",
+            "description":"Lorem ipsum lorem",
+            "rating": 1,
+            "image": Image10
+        }
+    ],
+    "categories":[
+        {
+            "id":1,
+            "name":"Beef",
+            "description":"Lorem ipsum lorem",
+            "rating":10,
+            "image": Image1
+        },
+        {
+            "id":2,
+            "name":"Pork",
+            "description":"Lorem ipsum lorem",
+            "rating": 20,
+            "image":Image2
+        },
+        {
+            "id":3,
+            "name":"Chicken" ,
+            "description":"Lorem ipsum lorem",
+            "rating": 30,
+            "image":Image3
+        },
+        {
+            "id":4,
+            "name":"Pizza",
+            "description":"Lorem ipsum lorem",
+            "rating": 40,
+            "image":Image4
+        },
+        {
+            "id":5,
+            "name":"Fastfood",
+            "description":"Lorem ipsum lorem",
+            "rating": 50,
+            "image":Image5
+        },
+        {
+            "id":6,
+            "name":"Asian",
+            "description":"Lorem ipsum lorem",
+            "rating": 60,
+            "image":Image6
+        },
+        {
+            "id":7,
+            "name":"Vegan",
+            "description":"Lorem ipsum lorem",
+            "rating": 70,
+            "image":Image7
+        },
+        {
+            "id":8,
+            "name":"Italian",
+            "description":"Lorem ipsum lorem",
+            "rating": 80,
+            "image":Image8
+        },
+        {
+            "id":9,
+            "name":"Bakery",
+            "description":"Lorem ipsum lorem",
+            "rating": 90,
+            "image":Image9
+        },
+        {
+            "id":10,
+            "name":"Japanese",
+            "description":"Lorem ipsum lorem",
+            "rating": 100,
+            "image":Image10
+        },
+        {
+            "id":11,
+            "name":"Chinese",
+            "description":"Lorem ipsum lorem",
+            "rating": 90,
+            "image":Image11
+        },
+        {
+            "id":12,
+            "name":"Burgers",
+            "description":"Lorem ipsum lorem",
+            "rating": 100,
+            "image":Image12
+        },
+        {
+            "id":13,
+            "name":"Snacks",
+            "description":"Lorem ipsum lorem",
+            "rating": 90,
+            "image":Image1
+        },
+        {
+            "id":14,
+            "name":"Beverages",
+            "description":"Lorem ipsum lorem",
+            "rating": 100,
+            "image":Image2
+        },
+        {
+            "id":15,
+            "name":"Bakery",
+            "description":"Lorem ipsum lorem",
+            "rating": 90,
+            "image":Image3
+        },
+        {
+            "id":16,
+            "name":"Breakfast and Brunch",
+            "description":"Lorem ipsum lorem",
+            "rating": 100,
+            "image":Image4
+        }
+    ],
+    "jewelry":[
+        {
+            "id":1,
+            "name":"Jewelry Restaurant 1",
+            "description":"Lorem ipsum lorem",
+            "rating":10   
+        },
+        {
+            "id":2,
+            "name":"Jewelry Restaurant 2",
+            "description":"Lorem ipsum lorem",
+            "rating":20   
+        },
+        {
+            "id":3,
+            "name":"Jewelry Restaurant 3",
+            "description":"Lorem ipsum lorem",
+            "rating":30   
+        },
+        {
+            "id":4,
+            "name":"Jewelry Restaurant 4",
+            "description":"Lorem ipsum lorem",
+            "rating":40   
+        },
+        {
+            "id":5,
+            "name":"Jewelry Restaurant 5",
+            "description":"Lorem ipsum lorem",
+            "rating":50   
+        },
+        {
+            "id":6,
+            "name":"Jewelry Restaurant 6",
+            "description":"Lorem ipsum lorem",
+            "rating":60   
+        },
+        {
+            "id":7,
+            "name":"Jewelry Restaurant 7",
+            "description":"Lorem ipsum lorem",
+            "rating":70   
+        },
+        {
+            "id":8,
+            "name":"Jewelry Restaurant 8",
+            "description":"Lorem ipsum lorem",
+            "rating":80   
+        },{
+            "id":9,
+            "name":"Jewelry Restaurant 9",
+            "description":"Lorem ipsum lorem",
+            "rating":90   
+        },
+        {
+            "id":10,
+            "name":"Jewelry Restaurant 10",
+            "description":"Lorem ipsum lorem",
+            "rating":100   
+        }
+    ],
+    "electronics":[
+        {
+            "id":1,
+            "name":"Electronics Restaurant 1",
+            "description":"Lorem ipsum lorem",
+            "rating":10   
+        },
+        {
+            "id":2,
+            "name":"Electronics Restaurant 2",
+            "description":"Lorem ipsum lorem",
+            "rating":20   
+        },
+        {
+            "id":3,
+            "name":"Electronics Restaurant 3",
+            "description":"Lorem ipsum lorem",
+            "rating":30   
+        },
+        {
+            "id":4,
+            "name":"Electronics Restaurant 4",
+            "description":"Lorem ipsum lorem",
+            "rating":40   
+        },
+        {
+            "id":5,
+            "name":"Electronics Restaurant 5",
+            "description":"Lorem ipsum lorem",
+            "rating":50   
+        },
+        {
+            "id":6,
+            "name":"Electronics Restaurant 6",
+            "description":"Lorem ipsum lorem",
+            "rating":60   
+        },
+        {
+            "id":7,
+            "name":"Electronics Restaurant 7",
+            "description":"Lorem ipsum lorem",
+            "rating":70   
+        },
+        {
+            "id":8,
+            "name":"Electronics Restaurant 8",
+            "description":"Lorem ipsum lorem",
+            "rating":80   
+        },
+        {
+            "id":9,
+            "name":"Electronics Restaurant 9",
+            "description":"Lorem ipsum lorem",
+            "rating":90   
+        },
+        {
+            "id":10,
+            "name":"Electronics Restaurant 10",
+            "description":"Lorem ipsum lorem",
+            "rating":100   
+        }
+    ]
+}
+export default data
